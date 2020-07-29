6771e6d5afc3f5870e9fafd0d37a1e75dd6d43d6
reneweiser <rene@encoding.group>
2020-07-29 14:36:37 +0200

    Initial setup

---
+++ src/components/Posts.svelte
@@ -0,0 +1,15 @@
+<script>
+  import axios from "axios";
+  let posts = {};
+  axios.get("http://robin.test/wp-json/wp/v2/posts").then((response) => {
+    posts = response.data;
+    console.log(posts);
+  });
+</script>
+
+<h2>Posts Component</h2>
+<ul>
+  {#each posts as post}
+    <li>{post.title.rendered}</li>
+  {/each}
+</ul>
