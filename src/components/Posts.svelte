27e9981fe81d7337ad7a29fcff85d85170556d45
moritzebeling <git@moritzebeling.com>
2020-07-29 21:17:47 +0200

    update posts component

--- src/components/Posts.svelte
+++ src/components/Posts.svelte
@@ -1,5 +1,6 @@
 <script>
   import { getContext } from "svelte";
+  import { Link } from "svelte-routing";
 
   let posts = {};
   let api = getContext("robinApi");
@@ -10,12 +11,38 @@
 </script>
 
 <h2>Posts Component</h2>
-<ul>
+<ol>
   {#if Object.keys(posts) != 0}
     {#each posts as post}
-      <li>{post.id} {post.title} {post.content}</li>
+
+      <li>
+        <Link to="/{post.id}" getProps={()=>({ title: post.title })}>
+          <div class="col indicator">•</div>
+          <div class="col date">{post.date}</div>
+          <h3 class="col title">{post.titleShort}</h3>
+          <div class="col client">{post.client}</div>
+          <div class="col discipline">{post.discipline.join(', ')}</div>
+        </Link>
+      </li>
+
     {/each}
   {:else}
     <li>Loading Posts</li>
   {/if}
-</ul>
+</ol>
+
+<style>
+
+  li {
+    border-bottom: 1px solid #fff;
+  }
+  li :global(a) {
+    display: flex;
+    padding: 0em 0.3em;
+  }
+  .col {
+    flex: 1 1 auto;
+    padding: 0.3em;
+  }
+
+</style>
