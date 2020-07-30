7d4f484655ee88d9d7f3eb174baba274a8826f27
reneweiser <rene@encoding.group>
2020-07-30 15:20:46 +0200

    Prepared separat route for the actual website

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,10 +1,9 @@
 <script>
   //   import RoutifyIntro from "./example/_components/RoutifyIntro.svelte";
-  import Home from "./portfolio/index.svelte";
   import { metatags } from "@sveltech/routify";
   metatags.title = "Portfolio";
   metatags.description = "Description coming soon...";
 </script>
 
 <!-- <RoutifyIntro /> -->
-<Home />
+<a href="/portfolio">Go to Portfolio</a>
---
+++ src/pages/portfolio/_components/Navigation.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { isActive, url } from "@sveltech/routify";
+  const links = [
+    ["./index", "Home"],
+    ["./info", "Info"],
+    ["./imprint", "Imprint"],
+    ["./archive", "Archive"],
+  ];
+</script>
+
+<nav>
+  {#each links as [path, name]}
+    <a class:active={$isActive(path)} href={$url(path)}>{name}</a>
+  {/each}
+</nav>
