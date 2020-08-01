2b3e34aec9a8fa83836c20ba75fdc70e93e25e4c
reneweiser <rene@encoding.group>
2020-08-01 19:48:22 +0200

    Added titleImage to view

--- src/pages/portfolio/_components/Grid.svelte
+++ src/pages/portfolio/_components/Grid.svelte
@@ -1,9 +1,7 @@
 <script>
-
   import { url } from "@sveltech/routify";
 
   export let posts = [];
-
 </script>
 
 <style type="text/scss">
@@ -18,14 +16,13 @@
     border-bottom: $border;
     flex: 0 0 50%;
     > a {
-        display: block;
-        padding: 0.5rem;
+      display: block;
+      padding: 0.5rem;
     }
-    &:nth-child(odd){
+    &:nth-child(odd) {
       border-right: $border;
     }
   }
-
 </style>
 
 <ul>
@@ -34,7 +31,11 @@
       <li>
         <a href={$url('../:post', { post: post.slug })}>
           <figure>
-            <img alt="Nice poster" src="" width="500" height="1000" />
+            <img
+              alt="Nice poster"
+              src={post.titleImage.large}
+              width={post.titleImage.largeWidth}
+              height={post.titleImage.largeHeight} />
           </figure>
         </a>
       </li>
