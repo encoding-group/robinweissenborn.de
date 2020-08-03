6d1833d95e3416f8e4b2259047f68a25e3b05455
moritzebeling <git@moritzebeling.com>
2020-08-03 18:01:01 +0200

    basic layout for info page

--- src/pages/portfolio/info/index.svelte
+++ src/pages/portfolio/info/index.svelte
@@ -6,17 +6,47 @@
   const wpAdapter = getContext("WordpressAdapter");
 </script>
 
-<h1>Info</h1>
+<style type="text/scss">
+  .panels {
+    display: flex;
+    section {
+      flex: 1 0 50%;
+      padding: 1.5rem;
+    }
+  }
+
+  address {
+    margin-bottom: 1em;
+  }
+</style>
+
 {#await wpAdapter.getPage('info')}
   <Message />
 {:then site}
   {(console.log(site), '')}
 
-  <p>
-    {site.contact.person}
-    <br />
-    {site.contact.email}
-  </p>
+  <div class="panels">
+    <section>
+
+      <h1>{site.contact.person}</h1>
+      <address>
+        {site.contact.street}<br />
+        {site.contact.zip}
+        {site.contact.city}<br />
+        {site.contact.country}
+      </address>
+
+      <p>
+        {site.contact.email}<br />
+        {site.contact.tel}
+      </p>
+
+    </section>
+    <section>
+      <h2>Visual Communication</h2>
+      {site.content}
+    </section>
+  </div>
 
 {:catch error}
   <Message>{error.message}</Message>
