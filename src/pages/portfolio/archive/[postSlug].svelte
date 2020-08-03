a3c958cbb585d54e0486bf4b38769560cebc0104
moritzebeling <git@moritzebeling.com>
2020-08-03 19:21:21 +0200

    add post info

--- src/pages/portfolio/archive/[postSlug].svelte
+++ src/pages/portfolio/archive/[postSlug].svelte
@@ -10,6 +10,23 @@
   const wpAdapter = getContext("WordpressAdapter");
 </script>
 
+<style type="text/scss">
+  article {
+    height: 100vh;
+    .headline {
+      padding: 1.5rem;
+      text-align: center;
+    }
+  }
+  .panels {
+    padding: 0 25%;
+    display: flex;
+    > div {
+      flex: 1 0 50%;
+      padding: 0.5em;
+    }
+  }
+</style>
 
 {#await wpAdapter.getPost(postSlug)}
   <Message />
@@ -17,6 +34,36 @@
   {(console.log(post), '')}
 
   <Gallery gallery={post.galleryGrid} />
+
+  <article>
+
+    <div class="headline">
+      <h3>Info</h3>
+    </div>
+
+    <div class="panels">
+      <div>
+        {#if post.year}
+          <p>{post.year}</p>
+        {/if}
+        {#if post.title}
+          <p>{post.title}</p>
+        {/if}
+        {#if post.client}
+          <p>{post.client.join(', ')}</p>
+        {/if}
+        {#if post.discipline}
+          <p>{post.discipline.join(', ')}</p>
+        {/if}
+      </div>
+      <div>
+        {#if post.content}
+              {@html post.content}
+        {/if}
+      </div>
+    </div>
+
+  </article>
 
 {:catch error}
   <Message>{error.message}</Message>
