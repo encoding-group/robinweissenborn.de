cbcfc718d274d3cb4e10c4f2487ad48d2f035912
moritzebeling <git@moritzebeling.com>
2020-08-03 17:03:40 +0200

    basic navigation

--- src/pages/portfolio/_components/Navigation.svelte
+++ src/pages/portfolio/_components/Navigation.svelte
@@ -1,15 +1,28 @@
 <script>
   import { isActive, url } from "@sveltech/routify";
-  const links = [
-    ["./index", "Home"],
-    ["./info", "Info"],
-    ["./imprint", "Imprint"],
-    ["./archive", "Archive"],
-  ];
 </script>
 
+<style type="text/scss">
+  nav {
+    position: sticky;
+    top: 0;
+    width: 100%;
+    padding: 1.5rem;
+    display: flex;
+    > div {
+      flex: 0 0 50%;
+      &.right {
+        text-align: right;
+      }
+    }
+  }
+</style>
+
 <nav>
-  {#each links as [path, name]}
-    <a class:active={$isActive(path)} href={$url(path)}>{name}</a>
-  {/each}
+  <div class="left">
+    <a class:active={$isActive("./index")} href={$url("./index")}>Robin Weißenborn</a>
+  </div>
+  <div class="right">
+    <a class:active={$isActive("./archive")} href={$url("./archive")}>Archive</a>
+  </div>
 </nav>
