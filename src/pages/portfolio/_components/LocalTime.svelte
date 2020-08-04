bad394709d21fc023e3b1fdd20dd6fedf3bcd209
reneweiser <rene@encoding.group>
2020-08-04 10:07:21 +0200

    Apply formatting

--- src/pages/portfolio/_components/LocalTime.svelte
+++ src/pages/portfolio/_components/LocalTime.svelte
@@ -1,26 +1,26 @@
 <script>
-  import { onMount } from 'svelte';
+  import { onMount } from "svelte";
 
-	let time = new Date();
+  let time = new Date();
 
-	// these automatically update when `time`
-	// changes, because of the `$:` prefix
-	$: hours = time.getHours();
-	$: minutes = time.getMinutes();
+  // these automatically update when `time`
+  // changes, because of the `$:` prefix
+  $: hours = time.getHours();
+  $: minutes = time.getMinutes();
   $: seconds = time.getSeconds();
 
   // https://svelte.dev/repl/a15e5bf484bf4eddafe68996d4235187?version=3.18.2
-  const pad = n => n < 10 ? `0${n}` : n;
+  const pad = (n) => (n < 10 ? `0${n}` : n);
 
-	onMount(() => {
-		const interval = setInterval(() => {
-			time = new Date();
-		}, 1000);
+  onMount(() => {
+    const interval = setInterval(() => {
+      time = new Date();
+    }, 1000);
 
-		return () => {
-			clearInterval(interval);
-		};
-	});
+    return () => {
+      clearInterval(interval);
+    };
+  });
 </script>
 
 <span>{pad(hours)}:{pad(minutes)}:{pad(seconds)}</span>
