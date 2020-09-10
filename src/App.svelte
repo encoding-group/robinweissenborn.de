c41574ff1966c44ac2f8d48256588005e2b79bca
moritzebeling <git@moritzebeling.com>
2020-09-10 11:58:06 +0200

    #13 add html metatags with placeholder content

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   import { Router } from "@sveltech/routify";
+  import { metatags } from '@sveltech/routify'
   import { routes } from "../.routify/routes";
   import { setContext } from "svelte";
   import WordpressAdapter from "./js/WordpressAdapter.js";
@@ -8,6 +9,18 @@
     "WordpressAdapter",
     new WordpressAdapter("https://api.robinweissenborn.de/wp-json/wp/v2")
   );
+
+  metatags.title = 'Robin Weißenborn'; // site.title
+  metatags.author = 'Robin Weißenborn'; // site.contact.name
+  metatags.description = 'Description...'; // site.description
+  metatags.keywords = 'tag1, tag2,'; // site.tags
+  metatags.generator = 'encoding.group';
+  metatags['geo.region'] = 'DE';
+
+  metatags['og:type'] = 'website';
+  metatags['og:url'] = 'https://robinweissenborn.de'; // site.url
+  metatags['og:image'] = 'image'; // site.image
+  metatags['og:locale'] = 'de_DE';
 </script>
 
 <style type="text/scss" global>
