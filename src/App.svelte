4029c191faa6c9a6b201848ee364d78182e34057
reneweiser <rene@encoding.group>
2020-09-19 15:35:59 +0200

    Resolves #13 - Pulling data from info page

--- src/App.svelte
+++ src/App.svelte
@@ -1,50 +1,69 @@
 <script>
   import { Router } from "@sveltech/routify";
-  import { metatags } from "@sveltech/routify";
+  import { metatags, ready } from "@sveltech/routify";
   import { routes } from "../.routify/routes";
+  import { processInfo } from "./js/wpResponseParser.js";
+  import { checkExternalLinks } from "./js/externalLinks.js";
 
-  import { checkExternalLinks } from './js/externalLinks.js';
+  let jsonld;
+  $: getData();
+  function getData() {
+    fetch("https://api.robinweissenborn.de/wp-json/wp/v2/pages?slug=info")
+      .then((response) => response.json())
+      .then((json) => {
+        getMeta(json[0]);
+        jsonld = getJsonLd(json[0])
+        $ready();
+      });
+  }
 
-  metatags.title = "Robin Weißenborn"; // site.title
-  metatags.author = "Robin Weißenborn"; // site.contact.name
-  metatags.description = "Description..."; // site.description
-  metatags.keywords = "tag1, tag2,"; // site.tags
-  metatags.generator = "encoding.group";
-  metatags["geo.region"] = "DE";
+  function getMeta(data) {
+    metatags.title = `${data.acf.contact.first_name} ${data.acf.contact.last_name}`;
+    metatags.author = `${data.acf.contact.first_name} ${data.acf.contact.last_name}`;
+    metatags.description = data.acf["website-metadata"].description;
+    metatags.keywords = data.acf["website-metadata"].keywords;
+    metatags.generator = "encoding.group";
+    metatags["geo.region"] = "DE";
 
-  metatags["og:type"] = "website";
-  metatags["og:url"] = "https://robinweissenborn.de"; // site.url
-  metatags["og:image"] = "image"; // site.image
-  metatags["og:locale"] = "de_DE";
+    metatags["og:type"] = "website";
+    metatags["og:url"] = "https://robinweissenborn.de"; // site.url
+    metatags["og:image"] = data.acf["website-metadata"].image;
+    metatags["og:locale"] = "de_DE";
+  }
 
-  let jsonld = {
-    "@context": "https://schema.org",
-    "@type": "Person",
-    workLocation: {
-      "@type": "PostalAddress",
-      addressLocality: "", // site.contact.address1.city,
-      postalCode: "", // site.contact.address1.zip,
-      streetAddress: "", // site.contact.address1.street
-    },
-    name: "", // site.contact.name,
-    alternateName: "Robin Weissenborn", // replace ß with ss
-    description: "", // site.description,
-    additionalType: "Artist",
-    email: "", // "mailto:"+site.contact.mail,
-    image: "", // site.image,
-    jobTitle: "", // "Graphic Designer",
-    url: "", // site.url
-  };
+  function getJsonLd(data) {
+    return JSON.stringify({
+      "@context": "https://schema.org",
+      "@type": "Person",
+      workLocation: {
+        "@type": "PostalAddress",
+        addressLocality: data.acf["address-1"].city,
+        postalCode: data.acf["address-1"].zip_code,
+        streetAddress: data.acf["address-1"].street,
+      },
+      name: `${data.acf.contact.first_name} ${data.acf.contact.last_name}`,
+      alternateName: `${data.acf.contact.first_name} ${data.acf.contact.last_name}`.replace(
+        /ß/g,
+        "ss"
+      ),
+      description: data.acf["website-metadata"].description,
+      additionalType: "Artist",
+      email: data.acf.contact.email,
+      image: data.acf["website-metadata"].image,
+      jobTitle: "Graphic Designer",
+      url: "https://robinweissenborn.de", // site.url
+    });
+  }
 </script>
 
 <style type="text/scss" global>
   @import "../static/global.css";
 </style>
 
-<svelte:window on:click={checkExternalLinks}></svelte:window>
+<svelte:window on:click={checkExternalLinks} />
 
 <svelte:head>
-  <!-- {@html `<script type="application/ld+json">${JSON.stringify(jsonld)}</script>`} -->
+  {@html '<script type="application/ld+json">' + jsonld + '</script>'}
 </svelte:head>
 
 <Router {routes} />
