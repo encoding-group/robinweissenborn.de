16d5039e9d715cadcba0c8e2afab3f814c4fab15
moritzebeling <git@moritzebeling.com>
2020-10-14 16:25:02 +0200

    layout for 404 page

--- src/pages/_fallback.svelte
+++ src/pages/_fallback.svelte
@@ -3,22 +3,12 @@
 </script>
 
 <style>
-  .huge {
-    font-size: 12rem;
-  }
-  .e404 {
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    text-align: center;
+  main {
+    margin: 3.8rem 1.5rem;
   }
 </style>
 
-<div class="e404">
-  <div class="huge">404</div>
-  <div class="big">Page not found. 
-  <!-- link to the parent folder of _fallback.svelte -->
-  <a href={$url('../')}>Go back</a>
-  </div>
-</div>
+<main>
+  <h2>404. Page not found</h2>
+  <a href={$url('/')}>&larr; Go back</a>
+</main>
